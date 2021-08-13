035f141b06e5d86b0998b4dd14d767aac7aa4f5d
EvandroOliveira495 <evandro.ricardo@hotmail.com>
2021-08-12 22:01:07 -0300

    exportando dados do componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { NovaTranferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTranferenciaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Output } from "@angular/core";
+
+@Component({
+    selector:'app-nova-transferencia',
+    templateUrl: './nova-transferencia.component.html',
+    styleUrls:['./nova-transferencia.component.scss']
+})
+export class NovaTranferenciaComponent{
+
+  @Output() aoTranferir = new EventEmitter<any>();
+
+  valor : number;
+  destino: number;
+
+  transferir(){
+    const valorEmitir = { valor: this.valor, destino: this.destino }
+    this.aoTranferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+
+}
